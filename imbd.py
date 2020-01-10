e0616b6f081adf0ff92218ddcbf039ea745c38ed
bloomsamurai16 <42292347+bloomsamurai16@users.noreply.github.com>
2020-01-10 22:58:07 +0400

    Update imbd.py

--- imbd.py
+++ imbd.py
@@ -23,4 +23,4 @@ reverse_word_index=dict([(value,key) for (key,value) in word_index.items()])
 def decode_review(text):
 	return ' '.join([reverse_word_index.get(i,"?") for i in text])
 
-print(decode_review(train_data[0
+print(decode_review(train_data[0]))
